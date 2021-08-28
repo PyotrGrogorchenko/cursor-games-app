9eceb9eff041a167828f34b83c3d5509e4d41c15
PyotrGrigorchenko <PePeGrigorchenko@yandex.ru>
2021-08-28 23:21:54 +0300

    deploy

--- src/services/api/db/const.ts
+++ src/services/api/db/const.ts
@@ -1,7 +1,5 @@
 export const urlRoot = IS_DEV ? `${document.location.protocol}//${document.location.hostname}:8000/api`
-  : process.env.API_URL
-console.log('urlRoot', urlRoot, 'IS_DEV', IS_DEV, 'process.env.API_URL', process.env.API_URL)
-console.log('process', process)
+  : 'https://cursor-games-api.herokuapp.com/api'
 
 export const url = {
   score: `${urlRoot}/score`,
