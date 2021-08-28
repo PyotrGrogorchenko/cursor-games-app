8f21c828f81fae62061bb4bf724518958c658ff1
PyotrGrigorchenko <PePeGrigorchenko@yandex.ru>
2021-08-28 22:35:34 +0300

    deploy

--- src/services/api/db/const.ts
+++ src/services/api/db/const.ts
@@ -1,6 +1,6 @@
 export const urlRoot = IS_DEV ? `${document.location.protocol}//${document.location.hostname}:8000/api`
   : process.env.API_URL
-console.log('urlRoot', urlRoot, 'IS_DEV', IS_DEV)
+console.log('urlRoot', urlRoot, 'IS_DEV', IS_DEV, 'process.env.API_URL', process.env.API_URL)
 
 export const url = {
   score: `${urlRoot}/score`,
