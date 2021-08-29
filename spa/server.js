eef58b874f19af025793d4423a3289e856ed5a8f
PyotrGrigorchenko <PePeGrigorchenko@yandex.ru>
2021-08-29 22:14:01 +0300

    favicon

--- spa/server.js
+++ spa/server.js
@@ -5,7 +5,7 @@ const favicon = require('serve-favicon')
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../dist')))
-app.use(favicon(path.join(__dirname, '../dist/static/main_logo_7vj_icon.ico')))
+app.use(favicon(path.join(__dirname, '../dist/static/main_logo.ico')))
 
 app.use('/', express.static(path.join(__dirname, '../dist/index.html')))
 app.get('*', (req, res) => {
